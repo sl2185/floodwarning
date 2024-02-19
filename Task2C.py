17bcd83ad31358e6e322b2627fc3acdc204ed112
Sean Lim <sl2185@cam.ac.uk>
2024-02-19 17:18:39 +0000

    fixed task 2c to import from new module

--- Task2C.py
+++ Task2C.py
@@ -1,4 +1,4 @@
-from floodsystem.station import stations_highest_rel_level, stations_level_over_threshold
+from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
 
 def run():
