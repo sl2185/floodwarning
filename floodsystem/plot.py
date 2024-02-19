9a4e5b1d53036bab0f78a4e0511cbcb87b269a84
Sean Lim <sl2185@cam.ac.uk>
2024-02-19 18:47:08 +0000

    changed implemenentation of plot.py back to take a single MonitoringStation object instead of a list

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
 
 def plot_water_levels(station, dates, levels):
     '''Plots and displays a plot of the water level data against time for a station, 
     includes typical low and high levels.
     Args:
-        Station: MonitoringStation obkect
+        Station: MonitoringStation object
         Dates: List of dates and times for corresponding water level readings
         Levels: list of level readings for specified dates
     '''
@@ -20,7 +19,7 @@ def plot_water_levels(station, dates, levels):
 
     plt.xlabel("Date and Time")
     plt.ylabel('water level (m)')
-    plt.xticks(rotation=45);
+    plt.xticks(rotation=45)
     plt.title(station.name) 
 
     # Display plot
